7aed8ab53493e2591c4ec5fe7d947f37ce24b766
castilloglenn <castilloglenn@ymail.com>
2024-04-13 22:06:11 +0800

    build: add setup file

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = ""
+with open("README.md", "r", encoding="utf-8") as f:
+    DESCRIPTION = f.read()
+
+setup(
+    name="dataflow",
+    version="0.1.0",
+    description="Python library that draws data flow animations.",
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Allen Glenn E. Castillo",
+    author_email="allenglenn.castillo@gmail.com",
+    url="https://github.com/castilloglenn/dataflow",
+    packages=find_packages(),
+    install_requires=["pygame", "watchdog"],
+    entry_points={
+        "console_scripts": [
+            "dataflow=dataflow.client.main:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    keywords="pygame watchdog live-coding development",
+    license="MIT",
+)
