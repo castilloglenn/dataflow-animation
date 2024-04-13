439e9280da11add7de7c164ff7f99f7432e6a191
castilloglenn <castilloglenn@ymail.com>
2024-04-13 23:26:33 +0800

    style: separated ScriptHandler

---
+++ dataflow/client/script_handler.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import importlib.util
+
+from watchdog.events import FileSystemEventHandler
+
+
+class ScriptHandler(FileSystemEventHandler):
+    def __init__(self, directory, filename, renderer):
+        self.directory = directory
+        self.filename = filename
+        self.path = os.path.join(directory, filename)
+
+        self.renderer = renderer
+        self.last_known_version = None
+
+    def on_modified(self, event):
+        if event.src_path == self.path:
+            self.load_and_run()
+
+    def load_and_run(self):
+        spec = importlib.util.spec_from_file_location(
+            "current_module",
+            self.path,
+        )
+        module = importlib.util.module_from_spec(spec)
+        try:
+            spec.loader.exec_module(module)
+            if hasattr(module, "Test"):
+                logging.info("File change detected. Running Test...")
+                module.Test()
+            elif hasattr(module, "Animation"):
+                logging.info("File change detected. Applying animation...")
+                self.renderer.set_animation(module.Animation)
+            else:
+                logging.error(
+                    "No Test or Animation class found in the script.",
+                )
+                self.renderer.set_animation(None)
+
+        # pylint: disable=W0718
+        except Exception as e:
+            logging.error("Error loading script:\n%s: %s", type(e).__name__, e)
+            self.renderer.set_animation(None)
