87421c3514f5b174b0c8cb142a6f989d2a5320b6
castilloglenn <castilloglenn@ymail.com>
2024-04-13 22:07:22 +0800

    feat: separate watchdog thread as daemon

--- dataflow/client/main.py
+++ dataflow/client/main.py
@@ -2,18 +2,22 @@ import os
 import sys
 import time
 import logging
+import threading
 import importlib.util
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+from dataflow.core.renderer import PygameRenderer
+
 
 class ScriptHandler(FileSystemEventHandler):
-    def __init__(self, directory, filename):
+    def __init__(self, directory, filename, renderer):
         self.directory = directory
         self.filename = filename
         self.path = os.path.join(directory, filename)
 
+        self.renderer = renderer
         self.last_known_version = None
 
     def on_modified(self, event):
@@ -31,17 +35,25 @@ class ScriptHandler(FileSystemEventHandler):
             if hasattr(module, "Test"):
                 logging.info("File change detected. Running Test...")
                 module.Test()
+            elif hasattr(module, "draw"):
+                logging.info("File change detected. Running draw...")
+                self.renderer.set_draw_func(module.draw)
             else:
-                logging.error("No Test class found in the script.")
+                logging.error(
+                    "No Test class or draw method found in the script.",
+                )
+                self.renderer.set_draw_func(None)
 
         # pylint: disable=W0718
         except Exception as e:
             logging.error("Error loading script:\n%s: %s", type(e).__name__, e)
+            self.renderer.set_draw_func(None)
 
 
-def main(path):
+def observe(path, renderer):
+    print("observe")
     directory, filename = os.path.split(path)
-    event_handler = ScriptHandler(directory, filename)
+    event_handler = ScriptHandler(directory, filename, renderer)
     observer = Observer()
     observer.schedule(event_handler, path=directory, recursive=False)
     observer.start()
@@ -54,23 +66,37 @@ def main(path):
     except KeyboardInterrupt:
         logging.info("Stopping watcher...")
     finally:
+        renderer.stop()
         observer.stop()
         observer.join()
         logging.info("Stopped watching.")
 
 
-if __name__ == "__main__":
+def setup(filepath):
+    renderer = PygameRenderer()
+    watcher_thread = threading.Thread(
+        target=observe, args=(filepath, renderer), daemon=True
+    )
+    watcher_thread.start()
+    renderer.init()
+
+    try:
+        renderer.run()
+    finally:
+        renderer.stop()
+
+
+def main():
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s] %(message)s",
         datefmt="%Y/%m/%d %H:%M:%S",
     )
-
+    print(sys.argv)
     if len(sys.argv) != 2:
         logging.error(
             "Invalid number of arguments. "
-            + "Usage: python "
-            + "<full_path_to_main.py> "
+            + "Usage: dataflow "
             + "<full_path_to_file_to_watch.py>",
         )
         sys.exit(1)
@@ -80,4 +106,4 @@ if __name__ == "__main__":
         logging.error("File does not exist: %s", path_arg)
         sys.exit(1)
 
-    main(path_arg)
+    setup(path_arg)
