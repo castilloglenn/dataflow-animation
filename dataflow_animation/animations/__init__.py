d2c0ef0affe9eec97356e27f6976ce07ce2decfa
castilloglenn <castilloglenn@ymail.com>
2024-04-14 20:40:23 +0800

    chore: ignore R0903

--- dataflow_animation/animations/__init__.py
+++ dataflow_animation/animations/__init__.py
@@ -3,6 +3,7 @@ from dataflow_animation.objects import Entity, Information
 from dataflow_animation.enums import Path
 
 
+# pylint: disable=R0903
 class AnimationStep:
     def __init__(self, entity: Entity, information: Information, path: Path):
         self.entity = entity
