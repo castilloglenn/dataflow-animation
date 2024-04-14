bb860d1fc5994c1578f863f48728309c4e3d6093
castilloglenn <castilloglenn@ymail.com>
2024-04-14 11:14:52 +0800

    style: reduce nestedness

--- dataflow_animation/core/renderer.py
+++ dataflow_animation/core/renderer.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import logging
 
 import pygame
 
@@ -14,35 +15,36 @@ class PygameRenderer:
         self.running = True
         self.animation: Optional[Dataflow] = None
 
+    def set_animation(self, animation):
+        self.animation = animation
+
     def init(self):
         pygame.init()
 
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
-        self.run()
 
-    def set_animation(self, animation):
-        self.animation = animation
-
-    def run(self):
         try:
-            while self.running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
-
-                self.screen.fill((0, 0, 0))
-                if self.animation:
-                    self.animation.play(self.animation, self.screen)
-
-                pygame.display.flip()
-                self.clock.tick(60)
-        except pygame.error:
-            pass
+            self.run()
+        except pygame.error as e:
+            logging.error("Pygame error: %s", e)
         except KeyboardInterrupt:
-            pass
+            logging.info("Pygame exiting...")
         finally:
             pygame.quit()
 
     def stop(self):
         self.running = False
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill((0, 0, 0))
+            if self.animation:
+                self.animation.play(self.animation, self.screen)
+
+            pygame.display.flip()
+            self.clock.tick(60)
