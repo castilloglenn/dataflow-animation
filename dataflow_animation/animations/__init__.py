615fe6322a54ad3cb798cdb00ede5441e0f65e84
castilloglenn <castilloglenn@ymail.com>
2024-04-14 22:14:27 +0800

    feat: animation step

--- dataflow_animation/animations/__init__.py
+++ dataflow_animation/animations/__init__.py
@@ -1,11 +1,30 @@
+from dataclasses import dataclass
+
 from dataflow_animation.objects import Entity, Information
 
 from dataflow_animation.enums import Path
 
 
-# pylint: disable=R0903
+@dataclass
 class AnimationStep:
-    def __init__(self, entity: Entity, information: Information, path: Path):
-        self.entity = entity
-        self.information = information
-        self.path = path
+    information: Information
+    entity: Entity
+    path: Path
+    duration: int
+
+    @property
+    def name(self):
+        return f"{self.information.name} -> {self.entity.name}"
+
+
+def animate(
+    collection,
+    *,
+    info: Information,
+    to: Entity,
+    path: Path = Path.LINEAR,
+    duration: int = 1000,
+):
+    information = collection.find_information(info)
+    entity = collection.find_entity(to)
+    collection.register(AnimationStep(information, entity, path, duration))
