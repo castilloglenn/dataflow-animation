175d06d46aa9d005e95b46f726d776156eab1978
castilloglenn <castilloglenn@ymail.com>
2024-04-14 23:32:33 +0800

    docs: add instructions

--- examples/basic.py
+++ examples/basic.py
@@ -6,8 +6,10 @@ from dataflow_animation.constants import CONFIG
 
 class Animation(Dataflow):
     def setup(self):
+        # Optional: Override configuration setting
         CONFIG.update(width=1000)
 
+        # Step 1: Create entities and informations
         Entity(self, name="Frontend", level=1)
         Entity(self, name="Backend", level=2)
         Information(
@@ -16,4 +18,8 @@ class Animation(Dataflow):
             starts_at="Frontend",
         )
 
+        # Step 2: Animate the data flow
         animate(self, info="Data", to="Backend")
+
+        # Final step: Save the file and run the animation
+        # Updating the file will apply the changes realtime in the window
