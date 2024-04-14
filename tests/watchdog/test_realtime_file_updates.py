493c060bc4664f74e1da9a723678d827c5790fbd
castilloglenn <castilloglenn@ymail.com>
2024-04-14 12:05:34 +0800

    chore: add second test function

--- tests/watchdog/test_realtime_file_updates.py
+++ tests/watchdog/test_realtime_file_updates.py
@@ -1,7 +1,12 @@
 class Test:
     def __init__(self):
         print("Test init")
+
         self.test()
+        self.test2()
 
     def test(self):
         print("Test function")
+
+    def test2(self):
+        print("Test2 function")
