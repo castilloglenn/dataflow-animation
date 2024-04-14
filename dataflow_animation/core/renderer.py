d2275cbd986620774dbdbefb6d17430776f5e315
castilloglenn <castilloglenn@ymail.com>
2024-04-14 14:35:13 +0800

    chore: integrate config

--- dataflow_animation/core/renderer.py
+++ dataflow_animation/core/renderer.py
@@ -1,33 +1,55 @@
+import os
 from typing import Optional
 import logging
 
+from pygame import Surface
 import pygame
 
+from dataflow_animation.config import get_config
 from dataflow_animation import Dataflow
 
 
+CONFIG = get_config()
+
+
 class PygameRenderer:
-    def __init__(self, width=800, height=600):
-        self.width = width
-        self.height = height
-        self.screen = None
-        self.clock = None
-        self.running = True
+    def __init__(self):
         self.animation: Optional[Dataflow] = None
+        self.screen: Surface = None
+        self.clock: pygame.time.Clock = None
+        self.running = True
 
     def set_animation(self, animation):
         self.animation = animation
 
+    def play_animation(self):
+        if self.animation is None:
+            return
+
+        try:
+            self.animation.play(self.animation, self.screen)
+
+        except pygame.error as e:
+            logging.error("Pygame error: %s", e)
+
+        # pylint: disable=W0718
+        except Exception as e:
+            logging.error(
+                "Error playing animation:\n%s: %s",
+                type(e).__name__,
+                e,
+            )
+            self.set_animation(None)
+
     def init(self):
+        os.environ["SDL_VIDEO_WINDOW_POS"] = CONFIG.sdl_video_window_pos
         pygame.init()
 
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((CONFIG.width, CONFIG.height))
         self.clock = pygame.time.Clock()
 
         try:
             self.run()
-        except KeyboardInterrupt:
-            logging.info("Pygame exiting...")
         finally:
             pygame.quit()
             logging.info("Pygame stopped.")
@@ -35,26 +57,26 @@ class PygameRenderer:
     def stop(self):
         self.running = False
 
-    def render(self):
-        self.screen.fill((0, 0, 0))
-        if self.animation:
-            self.animation.play(self.animation, self.screen)
-
-        pygame.display.flip()
-        self.clock.tick(60)
-
     def parse_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
 
-    def loop(self):
-        while self.running:
-            self.parse_events()
-            self.render()
+    def render(self):
+        self.screen.fill((0, 0, 0))
+        self.play_animation()
+
+        pygame.display.update()
+        self.clock.tick(CONFIG.fps)
 
     def run(self):
         try:
-            self.loop()
+            while self.running:
+                self.parse_events()
+                self.render()
+
         except pygame.error as e:
             logging.error("Pygame error: %s", e)
+
+        except KeyboardInterrupt:
+            logging.info("Pygame exiting...")
