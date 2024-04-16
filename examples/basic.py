fcb5ab4391dc9351e7ac70ce6dc7fa1ad0793eac
castilloglenn <castilloglenn@ymail.com>
2024-04-16 17:48:59 +0800

    chore: add generators and doc-ed previous

--- examples/basic.py
+++ examples/basic.py
@@ -15,18 +15,29 @@ class Animation(Dataflow):
         # )
 
         # Step 1: Create entities and informations
-        Entity(self, name="Web App", level=1)
-        Entity(self, name="Mobile App", level=1)
-        Entity(self, name="Backend", level=2)
+        # Example:
+        #
+        # Entity(self, name="Web App", level=1)
+        # Entity(self, name="Mobile App", level=1)
+        # Entity(self, name="Backend", level=2)
+        #
+        # Information(
+        #     self,
+        #     name="Request",
+        #     starts_at="Web App",
+        # )
+        # Generators for fun:
+        for i in range(5):
+            Entity(self, name=f"Server {i+1}", level=2)
 
-        Information(
-            self,
-            name="Request",
-            starts_at="Web App",
-        )
+        for i in range(3):
+            Entity(self, name=f"Client {i+1}", level=1)
+
+        Information(self, name="Request", starts_at="Client 3")
 
         # Step 2: Animate the data flow
-        animate(self, info="Request", to="Backend")
+        # animate(self, info="Request", to="Backend")
+        animate(self, info="Request", to="Server 1")
 
         # Final step: Save the file and run the animation
         # Updating the file will apply the changes realtime in the window
